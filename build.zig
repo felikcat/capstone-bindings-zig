adad08cbc56e6602c5d1decab0792dbdf076b5c4
felikcat <29991266+felikcat@users.noreply.github.com>
2025-01-09 09:36:39 -0800

    Only support Windows and x86_64

--- build.zig
+++ build.zig
@@ -1,12 +1,17 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{ .default_target = .{
+        .cpu_arch = .x86_64,
+        .os_tag = .windows,
+    } });
     const optimize = b.standardOptimizeOption(.{});
 
     const capstone = b.dependency("capstone", .{
         .target = target,
         .optimize = optimize,
+        .@"x86-att-disable" = true,
+        .@"support-only-target-arch" = true,
     });
     const compiled_capstone = capstone.artifact("capstone");
 
